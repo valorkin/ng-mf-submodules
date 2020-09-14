e4d44f26a0528698dfbfd27d2b9bb865b6794e35
Dmitriy Shekhovtsov <valorkin@gmail.com>
2020-09-14 17:18:35 +0300

    feat: ng9 and singleton dependencies (#3)

---
+++ packages/content-recommended-categories/src/app/recommended-categories/recommended-categories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recommended-categories',
+  templateUrl: './recommended-categories.component.html',
+  // styleUrls: ['./recommended-categories.component.css']
+})
+export class RecommendedCategoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
